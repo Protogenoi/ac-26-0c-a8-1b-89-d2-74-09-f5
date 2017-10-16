82b8910d024bab67c3349ab2c88d74137f7c7e47
Michael <michael@adl-crm.uk>
2017-10-16 16:07:37 +0100

    Updated Warlock gear.

--- W40K/views/enemy_factions/eldar-view.php
+++ W40K/views/enemy_factions/eldar-view.php
@@ -20,11 +20,77 @@
                         <option disabled>─────HEAVY SUPPORT─────</option>
                         <option value="Dark Reapers" <?php if($TARGET_UNIT=='Dark Reapers') { echo "selected"; } ?> >Dark Reapers</option>
                         <option value="Dark Reapers Exarch" <?php if($TARGET_UNIT=='Dark Reapers Exarch') { echo "selected"; } ?> >Dark Reapers Exarch</option>
+                        
                     </select>
                 </div>     
-            </div>                   
-                 
-                 
-                              </div>
+            </div> 
+             </div>
+         <?php if(isset($TARGET_UNIT)) { ?>
+             <br><br><br><br><br><br><br><br><br><br>
+              <div class="row">
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">Abilities, keywords, and faction keywords.</div>
+      <div class="panel-body">
+          
+<?php
+
+        if($ENEMY_FACTION=='Eldar') {
+           
+           require(__DIR__ . '/../../target_stats/eldar-stats.php');
+
+       }
+       
+       echo "<table class='table table-condensed'>
+           <tr>
+           <th colspan='10'>Unit Stats</th>
+           </tr>
+           <tr>
+           <th>M</th>
+           <th>WS</th>
+           <th>BS</th>
+           <th>S</th>
+           <th>T</th>
+           <th>W</th>
+           <th>A</th>
+           <th>Ld</th>
+           <th>Save</th>
+           <th>Invul</th>
+           </tr>
+           <tr>
+           <td>$T_MOVE</td>
+           <td>$T_WS</td>
+           <td>$T_BS</td>
+           <td>$T_STR</td>
+           <td>$T_TOUGHNESS</td>
+           <td>$T_WOUNDS</td>
+           <td>$T_ATTACKS</td>
+           <td>$T_LD</td>
+           <td>$T_SAVE</td>
+           <td>$T_INVUL</td>
+           </tr>
+           </table>";
+        
+        echo "<strong>Abilities:</strong><ul>";
+foreach($T_ABILITIES as $key) {
+    echo "<li>$key</li>";
+}
+
+echo "</ul><strong>Faction Keywords:</strong><ul>";
+foreach($T_FACTION_KW as $key) {
+    echo "<li>$key</li>";
+}
+echo "</ul><strong>Keywords:</strong><ul>";
+foreach($T_KEYWORDS as $key) {
+    echo "<li>$key</li>";
+}
+echo "</ul>";
+?>
+      
+      </div>
+    </div>
+  </div>
+              </div>
+         <?php } ?>
 
         </div>
