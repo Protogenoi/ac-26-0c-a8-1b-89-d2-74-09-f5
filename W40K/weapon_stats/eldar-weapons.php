5fabe3df897cab77ea6504a23498ef2d4afcf073
Michael <michael@adl-crm.uk>
2017-10-16 15:41:39 +0100

    Added Shuriken Pistol.

---
+++ W40K/weapon_stats/eldar-weapons.php
@@ -0,0 +1,12 @@
+<?php
+
+        if($UNIT_WEAPON=='Shuriken Pistol') {
+            $WEAPON_RANGE=12;
+            $WEAPON_TYPE='Pistol 1';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";
+            
+        $WEAPON_ABILITY="Each time you make a wound roll 6+ for this weapon, that hit is resolved with an AP of -3 instead of 0.";    
+        
+    }
