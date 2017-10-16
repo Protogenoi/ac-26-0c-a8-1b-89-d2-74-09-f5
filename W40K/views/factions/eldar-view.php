359c1d9f1eb921651a42a2b0a0a75159e53ba731
Michael <michael@adl-crm.uk>
2017-10-16 16:02:18 +0100

    Added Warlock wargear.

--- W40K/views/factions/eldar-view.php
+++ W40K/views/factions/eldar-view.php
@@ -55,30 +55,14 @@
                         <option value='Staff of Ulthamar'<?php if($UNIT_WEAPON=='Staff of Ulthamar') { echo "selected"; } ?> >Staff of Ulthamar</option>
                         <option value='Witchblade'<?php if($UNIT_WEAPON=='Witchblade') { echo "selected"; } ?> >Witchblade</option>
                             <?php } 
-                            if(strpos($UNIT,"Bezerker") !== false) {
+                            if(strpos($UNIT,"Warlock") !== false) {
                             ?>
                         <option disabled>─────Wargear─────</option>
-                        <option value='Bolt Pistol'<?php if($UNIT_WEAPON=='Bolt Pistol') { echo "selected"; } ?> >Bolt Pistol</option>
-                        <option value='Plasma Pistol' <?php if($UNIT_WEAPON=='Plasma Pistol') { echo "selected"; } ?> >Plasma Pistol</option>
-                        <option value='Supercharged Plasma Pistol' <?php if($UNIT_WEAPON=='Supercharged Plasma Pistol') { echo "selected"; } ?> >Supercharged Plasma Pistol</option>
-                        <option value='Frag Grenade' <?php if($UNIT_WEAPON=='Frag Grenade') { echo "selected"; } ?> >Frag Grenade</option>
-                        <option value='Krak Grenade' <?php if($UNIT_WEAPON=='Krak Grenade') { echo "selected"; } ?> >Krak Grenade</option>
+                        <option value='Singing Spear'<?php if($UNIT_WEAPON=='Singing Spear') { echo "selected"; } ?> >Singing Spear</option>
+                        <option value='Shuriken Pistol' <?php if($UNIT_WEAPON=='Shuriken Pistol') { echo "selected"; } ?> >Shuriken Pistol</option>
                         <option disabled>─────Melee─────</option>
-                        <option value='Chain Sword' <?php if($UNIT_WEAPON=='Chain Sword') { echo "selected"; } ?> >Chain Sword</option>
-                        <option value='Chainaxe' <?php if($UNIT_WEAPON=='Chainaxe') { echo "selected"; } ?> >Chainaxe</option>
-                        <option disabled>─────Champion Equipment─────</option>
-                        <option value='Boltgun' <?php if($UNIT_WEAPON=='Boltgun') { echo "selected"; } ?> >Boltgun</option>
-                        <option value='Combi-bolter'<?php if($UNIT_WEAPON=='Combi-bolter') { echo "selected"; } ?> >Combi-bolter</option> 
-                        <option value='Combi-flamer'<?php if($UNIT_WEAPON=='Combi-flamer') { echo "selected"; } ?> >Combi-flamer</option>                       
-                        <option value='Combi-melta'<?php if($UNIT_WEAPON=='Combi-melta') { echo "selected"; } ?> >Combi-melta</option>
-                        <option value='Combi-plasma'<?php if($UNIT_WEAPON=='Combi-plasma') { echo "selected"; } ?> >Combi-plasma</option>
-                        <option value='Supercharged Combi-plasma'<?php if($UNIT_WEAPON=='Supercharged Combi-plasma') { echo "selected"; } ?> >Supercharged Combi-plasma</option>
-                        <option disabled>─────Melee─────</option>
-                        <option value='Lighting Claw' <?php if($UNIT_WEAPON=='Lighting Claw') { echo "selected"; } ?> >Lighting Claw</option>
-                        <option value='Power Axe' <?php if($UNIT_WEAPON=='Power Axe') { echo "selected"; } ?> >Power Axe</option>
-                        <option value='Power Fist' <?php if($UNIT_WEAPON=='Power Fist') { echo "selected"; } ?> >Power Fist</option>
-                        <option value='Power Maul' <?php if($UNIT_WEAPON=='Power Maul') { echo "selected"; } ?> >Power Maul</option>
-                        <option value='Power Sword' <?php if($UNIT_WEAPON=='Power Sword') { echo "selected"; } ?> >Power Sword</option>
+                        <option value='Singing Spear (Melee)' <?php if($UNIT_WEAPON=='Singing Spear (Melee)') { echo "selected"; } ?> >Singing Spear (Melee)</option>
+                        <option value='Witchblade'<?php if($UNIT_WEAPON=='Witchblade') { echo "selected"; } ?> >Witchblade</option>
                             <?php }                              
                         } else { ?>
                         <option value=''>Select...</option>
@@ -86,15 +70,15 @@
                     </select>
                 </div>     
             </div>
-                                                                                                                                                                                                                                                                                                       
+                                                                                                                                                                                                                                                                                                                               
         <script>
 $(document).ready(function () {
     $("#UNIT").change(function () {
         var val = $(this).val();  
         if (val === "Eldrad Ulthran") {
             $("#UNIT_WEAPON").html("<option disabled>─────Wargear─────</option><option value='Shuriken Pistol'>Shuriken Pistol</option><option disabled>─────Melee─────</option><option value='Staff of Ulthamar'>Staff of Ulthamar</option><option value='Witchblade'>Witchblade</option>");
-        } else if (val === "Abaddon the Despoiler") {
-            $("#UNIT_WEAPON").html("<option disabled>─────Wargear─────</option><option value='Talon of Horus'>Talon of Horus</option><option disabled>─────Melee─────</option><option value='Talon of Horus (Melee)'>Talon of Horus (Melee)</option><option value='Drachnyen'>Drach'nyen</option>");
+        } else if (val === "Warlock") {
+            $("#UNIT_WEAPON").html("<option disabled>─────Wargear─────</option><option value='Singing Spear'>Singing Spear</option><option value='Shuriken Pistol'>Shuriken Pistol</option><option disabled>─────Melee─────</option><option value='Singing Spear (Melee)'>Singing Spear (Melee)</option><option value='Witchblade'>Witchblade</option>");
         } 
     });
 });        
