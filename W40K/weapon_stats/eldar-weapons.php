081591106ec2d9dc9b961997734987c5ac313c3b
Michael <michael@adl-crm.uk>
2017-10-16 15:48:18 +0100

    Updated Eldrad wargear.

--- W40K/weapon_stats/eldar-weapons.php
+++ W40K/weapon_stats/eldar-weapons.php
@@ -10,3 +10,24 @@
         $WEAPON_ABILITY="Each time you make a wound roll 6+ for this weapon, that hit is resolved with an AP of -3 instead of 0.";    
         
     }
+    
+            if($UNIT_WEAPON=='Staff of Ulthamar') {
+            $WEAPON_RANGE='Melee';
+            $WEAPON_TYPE='Melee';
+            $WEAPON_STR="2+";
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="1D3";
+                    
+    }
+    
+            if($UNIT_WEAPON=='Witch Blade') {
+            $WEAPON_RANGE='Melee';
+            $WEAPON_TYPE='Melee';
+            $WEAPON_STR="User";
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1D3";
+            
+        $WEAPON_ABILITY="This weapon always wounds on a 2+.";    
+            
+                    
+    }    
