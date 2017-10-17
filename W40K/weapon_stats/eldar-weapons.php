89234b3237a8a43a189bc794ea3aaf5fc2d9cfad
Michael <michael@adl-crm.uk>
2017-10-17 10:30:34 +0100

    Updated for Dark Reaper weapons.

--- W40K/weapon_stats/eldar-weapons.php
+++ W40K/weapon_stats/eldar-weapons.php
@@ -126,4 +126,52 @@
             
         $WEAPON_ABILITY="This weapon always wounds on a 2+.";    
         
-    }      
+    }    
+    
+        if($UNIT_WEAPON=='Aeldari Sunburst Missile Launcher') {
+            $WEAPON_RANGE=48;
+            $WEAPON_TYPE='Heavy 1D6';
+            $WEAPON_STR=4;
+            $WEAPON_AP=1;
+            $WEAPON_DAMAGE="1";
+                    
+    }  
+    
+        if($UNIT_WEAPON=='Aeldari Starshot Missile Launcher') {
+            $WEAPON_RANGE=48;
+            $WEAPON_TYPE='Heavy 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="1D6";
+                    
+    }  
+    
+        if($UNIT_WEAPON=='Reaper Starshot Missile Launcher') {
+            $WEAPON_RANGE=48;
+            $WEAPON_TYPE='Heavy 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="3";
+                    
+    } 
+    
+        if($UNIT_WEAPON=='Reaper Starswarm Missile Launcher') {
+            $WEAPON_RANGE=48;
+            $WEAPON_TYPE='Heavy 2';
+            $WEAPON_STR=5;
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="2";
+                    
+    }  
+    
+        if($UNIT_WEAPON=='Tempest Launcher') {
+            $WEAPON_RANGE=36;
+            $WEAPON_TYPE='Heavy 2D6';
+            $WEAPON_STR=4;
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="1";
+            
+            $WEAPON_ABILITY="This weapon can target units that are not visible to the bearer.";
+                    
+    }     
+    
