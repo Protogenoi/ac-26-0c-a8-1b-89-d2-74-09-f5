1a829023dbbfeacdf4beae038358b1323d3162fd
Michael <michael@adl-crm.uk>
2017-10-17 21:06:02 +0100

    Added Plaguereaper weapon.

---
+++ W40K/weapon_stats/deathguard-weapons.php
@@ -0,0 +1,13 @@
+<?php
+
+        if($UNIT_WEAPON=='Plaguereaper') {
+            $WEAPON_RANGE='Melee';
+            $WEAPON_TYPE='Melee';
+            $WEAPON_STR='+2';
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="3";     
+            
+            $WEAPON_ABILITY="Plague Weapon";
+        
+    }
+
