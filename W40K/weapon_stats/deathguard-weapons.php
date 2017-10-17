037cbbd82b82040c59c851b49f86440f9f6d6a85
Michael <michael@adl-crm.uk>
2017-10-17 21:13:06 +0100

    Updated for Deathguard: Malignant Plaguecaster

--- W40K/weapon_stats/deathguard-weapons.php
+++ W40K/weapon_stats/deathguard-weapons.php
@@ -7,7 +7,46 @@
             $WEAPON_AP=3;
             $WEAPON_DAMAGE="3";     
             
-            $WEAPON_ABILITY="Plague Weapon";
+            $WEAPON_ABILITY="Plague Weapon - You can reroll any wound rolls of 1 when attacking with a plague weapon.";
         
     }
+    
+        if($UNIT_WEAPON=='Bolt Pistol') {
+            $WEAPON_RANGE='12';
+            $WEAPON_TYPE='Pistol 1';
+            $WEAPON_STR="4";
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";           
+        
+    }
+
+          if($UNIT_WEAPON=='Corrupted Staff') {
+            $WEAPON_RANGE='Melee';
+            $WEAPON_TYPE='Melee';
+            $WEAPON_STR='+2';
+            $WEAPON_AP=1;
+            $WEAPON_DAMAGE="1D3";     
+            
+            $WEAPON_ABILITY="Plague Weapon - You can reroll any wound rolls of 1 when attacking with a plague weapon.";
+        
+    }  
+    
+         if($UNIT_WEAPON=='Blight Grenade') {
+            $WEAPON_RANGE=6;
+            $WEAPON_TYPE='Grenade D6';
+            $WEAPON_STR=3;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";  
+            
+        $WEAPON_ABILITY="Plague Weapon - You can reroll wound rolls of 1 for this weapon.";     
+            
+        } 
+        
+        if($UNIT_WEAPON=='Krak Grenade') {
+            $WEAPON_RANGE=6;
+            $WEAPON_TYPE='Grenade 1';
+            $WEAPON_STR=6;
+            $WEAPON_AP=1;
+            $WEAPON_DAMAGE="1D3";          
+        }        
 
