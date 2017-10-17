2774b569e3ddb9ea7d622c19a6d8a47a9e326ce4
Michael <michael@adl-crm.uk>
2017-10-17 14:03:16 +0100

    Added Necron weapons.

---
+++ W40K/weapon_stats/necron-weapons.php
@@ -0,0 +1,31 @@
+<?php
+
+        if($UNIT_WEAPON=='Gauntlet of Fire') {
+            $WEAPON_RANGE=8;
+            $WEAPON_TYPE='Assault 1D6';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";
+            
+        $WEAPON_ABILITY="This weapon automatically hits its target.";    
+        
+    }
+    
+        if($UNIT_WEAPON=='Staff of the Destroyer') {
+            $WEAPON_RANGE=18;
+            $WEAPON_TYPE='Assault 3';
+            $WEAPON_STR=6;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="2";
+                    
+    }  
+    
+        if($UNIT_WEAPON=='Staff of the Destroyer (Melee)') {
+            $WEAPON_RANGE='Melee';
+            $WEAPON_TYPE='Melee';
+            $WEAPON_STR='User';
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="2";
+                    
+    }    
+
