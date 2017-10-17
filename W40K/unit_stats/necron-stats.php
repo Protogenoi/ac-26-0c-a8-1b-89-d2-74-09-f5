f520cd0ea059ffc5fdc650605e9a5cf50ddfb0f7
Michael <michael@adl-crm.uk>
2017-10-17 16:54:32 +0100

    Added Necron Warriors.

--- W40K/unit_stats/necron-stats.php
+++ W40K/unit_stats/necron-stats.php
@@ -34,4 +34,22 @@
         $U_FACTION_KW=array("Necrons","DYNASTY");
         $U_KEYWORDS=array("Character","Infantry","Cryptec");            
         
-    }     
+    }
+    
+    if($UNIT=='Necron Warriors') {
+        $U_MOVE=5;
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=4;
+        $U_WOUNDS=1;
+        $U_ATTACKS=1;
+        $U_LD=10;
+        $U_SAVE=4; 
+        $U_INVUL =0;
+        
+        $U_ABILITIES=array("Reanimation Protocols");
+        $U_FACTION_KW=array("Necrons","DYNASTY");
+        $U_KEYWORDS=array("Infantry","Warriors");            
+        
+    }    
