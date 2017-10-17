e189438776700d5d0f9ce599d9ab907557bafd96
Michael <michael@adl-crm.uk>
2017-10-17 21:33:54 +0100

    Updated for Deathguard: Noxious Blightbringer

--- W40K/weapon_stats/deathguard-weapons.php
+++ W40K/weapon_stats/deathguard-weapons.php
@@ -48,5 +48,34 @@
             $WEAPON_STR=6;
             $WEAPON_AP=1;
             $WEAPON_DAMAGE="1D3";          
-        }        
+        }
+
+        if($UNIT_WEAPON=='Plasma Pistol') {
+            $WEAPON_RANGE='12';
+            $WEAPON_TYPE='Pistol 1';
+            $WEAPON_STR="7";
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1";           
+        
+    } 
+    
+        if($UNIT_WEAPON=='Supercharged Plasma Pistol') {
+            $WEAPON_RANGE='12';
+            $WEAPON_TYPE='Pistol 1';
+            $WEAPON_STR="8";
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="2";           
+        
+    }       
+    
+          if($UNIT_WEAPON=='Cursed Plague Bell') {
+            $WEAPON_RANGE='Melee';
+            $WEAPON_TYPE='Melee';
+            $WEAPON_STR='User';
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="2";     
+            
+            $WEAPON_ABILITY="Plague Weapon - You can reroll any wound rolls of 1 when attacking with a plague weapon.";
+        
+    } 
 
