3f089f5678c0890df1b890c642ef5376a1a28f6c
Michael <michael@adl-crm.uk>
2017-10-17 14:03:36 +0100

    Added Imotekh the Stormlord.

---
+++ W40K/unit_stats/necron-stats.php
@@ -0,0 +1,19 @@
+<?php
+
+    if($TARGET_UNIT=='Imotekh the Stormlord') {
+        $U_MOVE=5;
+        $U_WS=2;
+        $U_BS=2;
+        $U_STR=5;
+        $U_TOUGHNESS=5;
+        $U_WOUNDS=6;
+        $U_ATTACKS=3;
+        $U_LD=9;
+        $U_SAVE=2; 
+        $U_INVUL =4;
+        
+        $U_ABILITIES=array("Living Metal","Phase Shifter","Bloodswarm Necroscarabs","Lord of the Storm","My Will Be Done","Undying");
+        $U_FACTION_KW=array("Necrons","Sautekh");
+        $U_KEYWORDS=array("Character","Infantry","Overlord","Imotekh the Stormlord");            
+        
+    } 
