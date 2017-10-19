4a1e4b0985bca9e30b5ff17ef9c3ce68308594ef
Michael <michael@adl-crm.uk>
2017-10-19 13:23:04 +0100

    Updated for Orks:Boyz weapon options.

--- W40K/weapon_stats/ork-weapons.php
+++ W40K/weapon_stats/ork-weapons.php
@@ -1,5 +1,12 @@
 <?php
 
+if($WEAPON_TYPE=='Kombi-weapon with Rokkit Launcha') {
+    $WEAPON_TYPE='Rokkit Launcha';
+}
+if($WEAPON_TYPE=='Kombi-weapon with Skorcha') {
+    $WEAPON_TYPE='Skorcha';
+}
+
         if($UNIT_WEAPON=='Twin Big Shoota') {
             $WEAPON_RANGE=36;
             $WEAPON_TYPE='Assault 6';
@@ -55,3 +62,59 @@
         $WEAPON_ABILITY="Each time the bearer fights, it can make 1 additional attack with this weapon.";    
             
     }    
+    
+        if($UNIT_WEAPON=='Big Shoota') {
+            $WEAPON_RANGE=36;
+            $WEAPON_TYPE='Assault 3';
+            $WEAPON_STR=5;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1"; 
+        
+    }
+    
+        if($UNIT_WEAPON=='Rokkit Launcha') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Assault 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="3"; 
+        
+    }
+
+        if($UNIT_WEAPON=='Kustom Shoota') {
+            $WEAPON_RANGE=18;
+            $WEAPON_TYPE='Assault 4';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1"; 
+        
+    } 
+    
+        if($UNIT_WEAPON=='Skorcha') {
+            $WEAPON_RANGE=8;
+            $WEAPON_TYPE='Assault 1D6';
+            $WEAPON_STR=5;
+            $WEAPON_AP=1;
+            $WEAPON_DAMAGE="1"; 
+            
+            $WEAPON_ABILITY='This weapon automatically hits its target.';
+        
+    }     
+    
+        if($UNIT_WEAPON=='Big Choppa') {
+            $WEAPON_RANGE='Melee';
+            $WEAPON_TYPE='Melee';
+            $WEAPON_STR='+2';
+            $WEAPON_AP=1;
+            $WEAPON_DAMAGE="2"; 
+        
+    }  
+
+        if($UNIT_WEAPON=='Power Klaw') {
+            $WEAPON_RANGE='Melee';
+            $WEAPON_TYPE='Melee';
+            $WEAPON_STR='x2';
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1D3"; 
+        
+    }      
