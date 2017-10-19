34d182605016b533fcd09d75f1afed71bc0972ad
Michael <michael@adl-crm.uk>
2017-10-19 11:54:28 +0100

    Added Ghazghkull Thraka stats.

---
+++ W40K/target_stats/orks-stats.php
@@ -0,0 +1,20 @@
+<?php
+
+    if($TARGET_UNIT=='Ghazghkull Thraka') {
+        $T_MOVE=5;
+        $T_WS=2;
+        $T_BS=5;
+        $T_STR=6;
+        $T_TOUGHNESS=6;
+        $T_WOUNDS=8;
+        $T_ATTACKS=5;
+        $T_LD=8;
+        $T_SAVE=2; 
+        $T_INVUL =4;
+        
+        $T_ABILITIES=array("Ere We Go, Mob Rule","Prophet of Gork and Mork","Great Waaagh!","The Boss is Watchin’");
+        $T_FACTION_KW=array("Ork","Goff");
+        $T_KEYWORDS=array("Character","Infantry","Mega Armour","Warboss","Ghazghkull Thraka");            
+        
+    } 
+
