aa2e7eca780693bc74df4101e174e79851c3f8dd
Michael <michael@adl-crm.uk>
2017-10-19 12:40:16 +0100

    Updated for Orks:Boyz.

--- W40K/unit_stats/orks-stats.php
+++ W40K/unit_stats/orks-stats.php
@@ -12,9 +12,27 @@
         $U_SAVE=2; 
         $U_INVUL =4;
         
-        $U_ABILITIES=array("Ere We Go, Mob Rule","Prophet of Gork and Mork","Great Waaagh!","The Boss is Watchin’");
+        $U_ABILITIES=array("Ere We Go, Mob Rule","Prophet of Gork and Mork","Great Waaagh!","The Boss is Watchin");
         $U_FACTION_KW=array("Ork","Goff");
         $U_KEYWORDS=array("Character","Infantry","Mega Armour","Warboss","Ghazghkull Thraka");            
         
     } 
+    
+    if($UNIT=='Boyz') {
+        $U_MOVE=5;
+        $U_WS=3;
+        $U_BS=5;
+        $U_STR=4;
+        $U_TOUGHNESS=4;
+        $U_WOUNDS=1;
+        $U_ATTACKS=2;
+        $U_LD=6;
+        $U_SAVE=6; 
+        $U_INVUL =0;
+        
+        $U_ABILITIES=array("Ere We Go, Mob Rule","Green Tide");
+        $U_FACTION_KW=array("Ork","CLAN");
+        $U_KEYWORDS=array("Infantry","Boyz");            
+        
+    }     
 
