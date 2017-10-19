95830d7b208ba5f82fed2e42d479c3dfb7dfc8a6
Michael <michael@adl-crm.uk>
2017-10-19 12:01:22 +0100

    Updated Orks: Ghazghkull Thraka wargear.

---
+++ W40K/weapon_stats/ork-weapons.php
@@ -0,0 +1,28 @@
+<?php
+
+        if($UNIT_WEAPON=='Twin Big Shoota') {
+            $WEAPON_RANGE=36;
+            $WEAPON_TYPE='Assault 6';
+            $WEAPON_STR=5;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1"; 
+        
+    }
+    
+        if($UNIT_WEAPON=='Kustom Klaw') {
+            $WEAPON_RANGE='Melee';
+            $WEAPON_TYPE='Melee';
+            $WEAPON_STR='x2';
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="3"; 
+        
+    }    
+
+        if($UNIT_WEAPON=='Stickbomb') {
+            $WEAPON_RANGE=6;
+            $WEAPON_TYPE='Grenade 1D6';
+            $WEAPON_STR=3;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1"; 
+        
+    }    
