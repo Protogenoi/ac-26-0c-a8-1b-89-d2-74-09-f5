9961f1897542067c1a4f990ecef53f6a69e65a2e
Michael <michael@adl-crm.uk>
2017-10-24 14:31:50 +0100

    Updated for Space Marine: Dreadnought

--- W40K/weapon_stats/ultramarines-weapons.php
+++ W40K/weapon_stats/ultramarines-weapons.php
@@ -513,3 +513,39 @@
         $WEAPON_ABILITY="This weapon automaticaly hits its target.";     
             
         }     
+        
+        if($UNIT_WEAPON=='Twin Lascannon') {
+            $WEAPON_RANGE=48;
+            $WEAPON_TYPE='Heavy 2';
+            $WEAPON_STR=9;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1D6";      
+            
+        } 
+
+        if($UNIT_WEAPON=='Heavy Plasma Cannon') {
+            $WEAPON_RANGE=36;
+            $WEAPON_TYPE='Heavy 1D3';
+            $WEAPON_STR=7;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1";      
+            
+        }
+
+        if($UNIT_WEAPON=='Supercharged Heavy Plasma Cannon') {
+            $WEAPON_RANGE=36;
+            $WEAPON_TYPE='Heavy 1D3';
+            $WEAPON_STR=8;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="2";      
+            
+        } 
+        
+        if($UNIT_WEAPON=='Dreadnought Combat Weapon') {
+            $WEAPON_RANGE='Melee';
+            $WEAPON_TYPE='Melee';
+            $WEAPON_STR='x2';
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="3";      
+            
+        }         
