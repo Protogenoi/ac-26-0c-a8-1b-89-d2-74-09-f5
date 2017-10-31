15136da3afd6c8fa4ba35d27b6e777ba7a8b65fa
Michael <michael@adl-crm.uk>
2017-10-31 16:37:20 +0000

    Updated for unsaved wounds.

---
+++ W40K/views/odds/odds-view.php
@@ -0,0 +1,21 @@
+    <table class='table table-condensed'>
+        <tr>
+        <th colspan='7'><?php echo "$this->BS+ to hit | $this->WOUNDS_ON to wound | $this->SAVE to save";?></th>
+        </tr>
+	<tr>
+	<th>Hits</th>
+	<th>Wounds</th>
+	<th>Unsaved Wounds</th>
+	<th>Invuls</th>
+	</tr>
+	<tr>
+	<th><?php echo "$this->CHANCE_TO_HIT% [$this->SHOW_HITS]" ?></th>
+	<th><?php echo "$this->CHANCE_TO_WOUND% [$this->SHOW_WOUNDS]" ?></th>
+	<th><?php echo "$this->CHANCE_TO_UNSAVE% [$this->SHOW_UNSAVE]" ?></th>
+	<th></th>
+	<th></th>
+	<th></th>
+        <th></th>    
+	</tr>
+	</table>
+
